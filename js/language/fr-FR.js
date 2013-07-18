4ea3c1d2089f5fc875c4ab2ca0aca37ca1e25156
Sergey Romanov <serg4172@mail.ru>
2013-07-18 21:57:30 +0600

    Deleet from franch language

--- js/language/fr-FR.js
+++ js/language/fr-FR.js
@@ -45,12 +45,3 @@ var language = {
 	d5: 'Vendredi',
 	d6: 'Samedi'
 };
-
-if(!String.prototype.format) {
-	String.prototype.format = function() {
-		var args = arguments;
-		return this.replace(/{(\d+)}/g, function(match, number) {
-			return typeof args[number] != 'undefined' ? args[number] : match;
-		});
-	};
-}
