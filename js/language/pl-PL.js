82a0cc04df7ff576714ad118eaae1e86e03526b6
sandlbn <marcin@spoczynski.com>
2013-08-19 20:52:04 +0200

    pl-PL Translation

---
+++ js/language/pl-PL.js
@@ -0,0 +1,53 @@
+if(!window.calendar_languages) {
+	window.calendar_languages = {};
+}
+window.calendar_languages['pl-PL'] = {
+	error_noview: 'Kalendarz: Widok {0} nie znaleziony',
+	error_dateformat: 'Kalendarz: Zły format daty {0}. Powinna być w formacie "now" lub "yyyy-mm-dd"',
+	error_loadurl: 'Kalendarz: Adres URL do ładowania wydarzeń nie ustawiony',
+	error_where: 'Kalendarz: Zły kierunek nawigacji {0}. Może być tylko "następny" or "poprzedni" or "dzisiaj"',
+
+	title_year: 'Rok {0}',
+	title_month: '{0} rok {1}',
+	title_week: '{0} tydzień roku {1}',
+	title_day: '{0} {1} {2} rok {3}',
+
+	week:'Tydzień',
+
+	m0: 'January',
+	m1: 'February',
+	m2: 'March',
+	m3: 'April',
+	m4: 'May',
+	m5: 'June',
+	m6: 'July',
+	m7: 'August',
+	m8: 'September',
+	m9: 'October',
+	m10: 'November',
+	m11: 'December',
+
+    ms0: 'Jan',
+    ms1: 'Feb',
+    ms2: 'Mar',
+    ms3: 'Apr',
+    ms4: 'May',
+    ms5: 'Jun',
+    ms6: 'Jul',
+    ms7: 'Aug',
+    ms8: 'Sep',
+    ms9: 'Oct',
+    ms10: 'Nov',
+    ms11: 'Dec',
+
+	d0: 'Sunday',
+	d1: 'Monday',
+	d2: 'Tuesday',
+	d3: 'Wednesday',
+	d4: 'Thursday',
+	d5: 'Friday',
+	d6: 'Saturday',
+
+	easter: 'Wielkanoc',
+	easterMonday: 'Poniedziałek wielkanocny'
+};
