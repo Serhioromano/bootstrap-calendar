fccf67dbbe2cbd1afe139c67a8ea4b5be7d37cf7
Sergey Romanov <serg4172@mail.ru>
2013-09-05 17:23:14 +0600

    Restore lost changes
    I do not know why but I had very seriose conflict when try to push some
    changes through GitHub for MAC. I spent sometime to make it through but
    since I am not git expert it happen that last to PR merges was lost. I
    restore it manually.

--- js/calendar.js
+++ js/calendar.js
@@ -125,13 +125,13 @@ if(!String.prototype.format) {
     var strings = {
         error_noview: 'Calendar: View {0} not found',
         error_dateformat: 'Calendar: Wrong date format {0}. Should be either "now" or "yyyy-mm-dd"',
-        error_loadurl: 'Calendar: Events load URL is not set',
+        error_loadurl: 'Calendar: Event URL is not set',
         error_where: 'Calendar: Wrong navigation direction {0}. Can be only "next" or "prev" or "today"',
 
-        title_year: 'Year {0}',
-        title_month: '{0} year {1}',
-        title_week: '{0} week of year {1}',
-        title_day: '{0} {1} {2} year {3}',
+        title_year: '{0}',
+        title_month: '{0} {1}',
+        title_week: 'week {0} of {1}',
+        title_day: '{0} {1} {2}, {3}',
 
         week:'Week',
 
@@ -270,10 +270,7 @@ if(!String.prototype.format) {
 
                 event.start_day = new Date(parseInt(event.start)).getDay();
                 if(self.options.first_day == 1) {
-                    event.start_day = event.start_day - 1;
-                }
-                if(self.options.start_day < 0) {
-                    event.start_day = 0;
+                    event.start_day = (event.start_day + 6) % 7;
                 }
                 if((event.end - event.start) <= 86400000) {
                     event.days = 1;
