f234029af51b09a3fe4eb8fbbaa64229449b3cc8
Michele Locati <michele@locati.it>
2013-10-09 08:17:15 +0200

    Only TABs in app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function($) {
 
-    "use strict";
+	"use strict";
 
 	var options = {
 		events_source: 'events.json.php',
@@ -33,7 +33,7 @@
 		}
 	};
 
-	var	 calendar = $('#calendar').calendar(options);
+	var calendar = $('#calendar').calendar(options);
 
 	$('.btn-group button[data-calendar-nav]').each(function() {
 		var $this = $(this);
@@ -49,20 +49,20 @@
 		});
 	});
 
-    $('#first_day').change(function(){
-        var value = $(this).val();
-        value = value.length ? parseInt(value) : null;
-        calendar.setOptions({first_day: value});
-        calendar.view();
-    });
+	$('#first_day').change(function(){
+		var value = $(this).val();
+		value = value.length ? parseInt(value) : null;
+		calendar.setOptions({first_day: value});
+		calendar.view();
+	});
 
-    $('#language').change(function(){
-        calendar.setLanguage($(this).val());
-        calendar.view();
-    });
+	$('#language').change(function(){
+		calendar.setLanguage($(this).val());
+		calendar.view();
+	});
 
 	$('#events-in-modal').change(function(){
 		var val = $(this).is(':checked') ? $(this).val() : null;
 		calendar.setOptions({modal: val});
-    });
+	});
 }(jQuery));
