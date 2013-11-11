ec11ce18dca90e56aa44ff325695bb313d1fe33b
Iulian Mihai <imihai@exacttarget.com>
2013-11-10 20:36:59 -0500

    Refactored to eliminate jsHint errors for strict programming: Eg: - added semicolons where missed - modified the var names to use CamelCase - modified if statements to add === instead of == - added verification in for (key in object)  statement to check for hasOwnProperty - added indentation to entire code of 4 spaces

--- js/language/template.js
+++ js/language/template.js
@@ -1,32 +1,32 @@
 // If you want to suggest a new language you can use this file as a template.
 // To reduce the file size you should remove the comment lines (the ones that start with // )
-if(!window.calendar_languages) {
-	window.calendar_languages = {};
+if(!window.calendarLanguages) {
+	window.calendarLanguages = {};
 }
 // Here you define the language and Country code. Replace en-US with your own.
 // First letters: the language code (lower case). See http://www.loc.gov/standards/iso639-2/php/code_list.php
 // Last letters: the Country code (upper case). See http://www.iso.org/iso/home/standards/country_codes/country_names_and_code_elements.htm
-window.calendar_languages['en-US'] = {
-	error_noview: 'Calendar: View {0} not found',
-	error_dateformat: 'Calendar: Wrong date format {0}. Should be either "now" or "yyyy-mm-dd"',
-	error_loadurl: 'Calendar: Event URL is not set',
-	error_where: 'Calendar: Wrong navigation direction {0}. Can be only "next" or "prev" or "today"',
+window.calendarLanguages['en-US'] = {
+	errorNoView: 'Calendar: View {0} not found',
+	errorDateFormat: 'Calendar: Wrong date format {0}. Should be either "now" or "yyyy-mm-dd"',
+	errorLoadUrl: 'Calendar: Event URL is not set',
+	errorWhere: 'Calendar: Wrong navigation direction {0}. Can be only "next" or "prev" or "today"',
 
-	no_events_in_day: 'No events in this day.',
+	noEventsInDay: 'No events in this day.',
 
 	// {0} will be replaced with the year (example: 2013)
-	title_year: '{0}',
+	titleYear: '{0}',
 	// {0} will be replaced with the month name (example: September)
 	// {1} will be replaced with the year (example: 2013)
-	title_month: '{0} {1}',
+	titleMonth: '{0} {1}',
 	// {0} will be replaced with the week number (example: 37)
 	// {1} will be replaced with the year (example: 2013)
-	title_week: 'week {0} of {1}',
+	titleWeek: 'week {0} of {1}',
 	// {0} will be replaced with the weekday name (example: Thursday)
 	// {1} will be replaced with the day of the month (example: 12)
 	// {2} will be replaced with the month name (example: September)
 	// {3} will be replaced with the year (example: 2013)
-	title_day: '{0} {1} {2}, {3}',
+	titleDay: '{0} {1} {2}, {3}',
 
 	week:'Week',
 
@@ -65,7 +65,7 @@ window.calendar_languages['en-US'] = {
 	d6: 'Saturday',
 
 	// Which is the first day of the week (2 for sunday, 1 for monday)
-	first_day: 2,
+	firstDay: 2,
 
 	// The list of the holidays.
 	// Each holiday has a date definition and a name (in your language)
