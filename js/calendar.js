07791ccf0b7b5f5bf69d1345cf5348495b9349db
Sandeep Pathivada <sandeep.pathivada@openapp.ie>
2014-04-01 11:49:02 +0100

    rename template files so that they will not conflict with server side templating engines like zope/plone

--- js/calendar.js
+++ js/calendar.js
@@ -899,7 +899,7 @@ if(!String.prototype.formatNum) {
 		}
 		var self = this;
 		$.ajax({
-			url:      this.options.tmpl_path + name + '.html',
+			url:      this.options.tmpl_path + name + '.tmpl',
 			dataType: 'html',
 			type:     'GET',
 			async:    false,
