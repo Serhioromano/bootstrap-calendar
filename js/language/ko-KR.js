80f2c64d62ec54b8efe1b435900bba6f711f9b06
blueooh <blueooh@taste-good.com>
2014-04-24 11:02:29 +0900

    remove comments

--- js/language/ko-KR.js
+++ js/language/ko-KR.js
@@ -71,27 +71,4 @@ window.calendar_languages['ko-KR'] = {
 	 	'03-10': '개천절',
 	 	'25-12': '크리스마스'
 	 }
-
-	// The format of the date may be one of the following:
-	// # For a holiday recurring every year in the same day: 'dd-mm' (dd is the day of the month, mm is the month). For example: '25-12'.
-	// # For a holiday that exists only in one specific year: 'dd-mm-yyyy' (dd is the day of the month, mm is the month, yyyy is the year). For example: '31-01-2013'
-	// # For Easter: use simply 'easter'
-	// # For holidays that are based on the Easter date: 'easter+offset in days'.
-	//   Some examples:
-	//   - 'easter-2' is Good Friday (2 days before Easter)
-	//   - 'easter+1' is Easter Monday (1 day after Easter)
-	//   - 'easter+39' is the Ascension Day
-	//   - 'easter+49' is Pentecost
-	// # For holidays that are on a specific weekday after the beginning of a month: 'mm+n*w', where 'mm' is the month, 'n' is the ordinal position, 'w' is the weekday being 0: Sunday, 1: Monday, ..., 6: Saturnday
-	//   For example:
-	//   - Second (2) Monday (1) in October (10): '10+2*1'
-	// # For holidays that are on a specific weekday before the ending of a month: 'mm-n*w', where 'mm' is the month, 'n' is the ordinal position, 'w' is the weekday being 0: Sunday, 1: Monday, ..., 6: Saturnday
-	//   For example:
-	//   - Last (1) Saturnday (6) in Match (03): '03-1*6'
-	//   - Last (1) Monday (1) in May (05): '05-1*1'
-	// # You can also specify a holiday that lasts more than one day. To do that use the format 'start>end' where 'start' and 'end' are specified as above.
-	//   For example:
-	//   - From 1 January to 6 January: '01-01>06-01'
-	//   - Easter and the day after Easter: 'easter>easter+1'
-	//   Limitations: currently the multi-day holydays can't cross an year. So, for example, you can't specify a range as '30-12>01-01'; as a workaround you can specify two distinct holidays (for instance '30-12>31-12' and '01-01'). 
 };
